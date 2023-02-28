388d8bb97b4e8d272322efdf26d9288483bfc559
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-02-28 17:17:26 -0300

    configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '506085731720152',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '6369f79f6126c1882d0310df02e97b62'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
