42df7c74e83471ce80e8eaf1a875eb297be9d70a
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-03-27 13:43:15 -0300

    rename JwtTokenGenerator to JwtTokenHandler

--- src/infra/crypto/jwt-token-handler.ts
+++ src/infra/crypto/jwt-token-handler.ts
@@ -5,10 +5,10 @@ import { sign } from 'jsonwebtoken'
 type Params = TokenGenerator.Params
 type Result = TokenGenerator.Result
 
-export class JwtTokenGenerator implements TokenGenerator {
+export class JwtTokenHandler implements TokenGenerator {
   constructor (private readonly secret: string) {}
 
-  async generateToken ({ expirationInMs, key }: TokenGenerator.Params): Promise<TokenGenerator.Result> {
+  async generateToken ({ expirationInMs, key }: Params): Promise<Result> {
     const expirationInSeconds = expirationInMs / 1000
     return sign({ key: key }, this.secret, { expiresIn: expirationInSeconds })
   }
