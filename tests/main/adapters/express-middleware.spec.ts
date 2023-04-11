8ce0b9ba79fed4f4b592ab0f95082e36a4a31193
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-04-11 14:15:15 -0300

    ensure ExpressMiddleware respond with correct error and statusCode

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -6,7 +6,8 @@ import { mock, MockProxy } from 'jest-mock-extended'
 type Adapter = (middleware: Middleware) => RequestHandler
 
 const adaptExpressMidleware: Adapter = middleware => async (req, res, next) => {
-  await middleware.handle({ ...req.headers })
+  const { statusCode, data } = await middleware.handle({ ...req.headers })
+  res.status(statusCode).json(data)
 }
 
 interface Middleware {
@@ -25,6 +26,10 @@ describe('ExpressMiddleware', () => {
     res = getMockRes().res
     next = getMockRes().next
     middleware = mock<Middleware>()
+    middleware.handle.mockResolvedValue({
+      statusCode: 500,
+      data: { error: 'any_error' }
+    })
   })
 
   beforeEach(() => {
@@ -44,6 +49,15 @@ describe('ExpressMiddleware', () => {
     await sut(req, res, next)
 
     expect(middleware.handle).toHaveBeenCalledWith({})
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('should respond with correct error and statusCode', async () => {
+    await sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(middleware.handle).toHaveBeenCalledTimes(1)
   })
 })
