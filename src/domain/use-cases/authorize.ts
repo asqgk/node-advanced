05723e77b24f9e34f64295991c1797d296ada405
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-04-18 16:34:51 -0300

    remove anti pattern middle man

--- src/domain/use-cases/authorize.ts
+++
@@ -1,8 +0,0 @@
-import { TokenValidator } from '@/domain/contracts/crypto'
-
-type Setup = (crypto: TokenValidator) => Authorize
-export type Authorize = (params: { token: string }) => Promise<string>
-
-export const setupAuthorize: Setup = (crypto) => async params => {
-  return crypto.validateToken(params)
-}
