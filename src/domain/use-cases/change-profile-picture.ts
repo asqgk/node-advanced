7b19dc02ba534658f4c59b2c92585f9768364e05
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-04-25 10:34:49 -0300

    move production code to their own files

---
+++ src/domain/use-cases/change-profile-picture.ts
@@ -0,0 +1,9 @@
+import { UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
+
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
+type Input = { id: string, file: Buffer }
+export type ChangeProfilePicture = (input: Input) => Promise<void>
+
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
+  await fileStorage.upload({ file, key: crypto.uuid(id) })
+}
