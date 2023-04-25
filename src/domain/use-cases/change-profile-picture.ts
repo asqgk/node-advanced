abe57ac985db53f4bbb8ed3bda840ed36e64dd6a
Francisco dos Passos <francisco.nedir@squidit.com.br>
2023-04-25 10:53:26 -0300

    ensure ChangeProfilePicture calls SaveUserPicture with correct input

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,11 +1,13 @@
 import { UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
+import { SaveUserPicture } from '@/domain/contracts/repos'
 
-type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture) => ChangeProfilePicture
 type Input = { id: string, file?: Buffer }
 export type ChangeProfilePicture = (input: Input) => Promise<void>
 
-export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => async ({ id, file }) => {
   if (file !== undefined) {
-    await fileStorage.upload({ file, key: crypto.uuid(id) })
+    const pictureUrl = await fileStorage.upload({ file, key: crypto.uuid(id) })
+    await userProfileRepo.savePicture({ pictureUrl })
   }
 }
