780b85143a7c30d7ae7af7dcf228d25b5c7e062d
Francisco Passos <chico.81@hotmail.com>
2023-06-02 16:16:24 -0300

    fix required validations tests

--- tests/application/validation/required.spec.ts
+++ tests/application/validation/required.spec.ts
@@ -2,31 +2,23 @@ import { RequiredFieldError } from '@/application/errors'
 import { Required, RequiredString, RequiredBuffer } from '@/application/validation'
 
 describe('Required', () => {
-  // it('should return RequiredFieldError if value is empty', () => {
-  //   const sut = new Required('', 'any_field')
+  it('should return RequiredFieldError if value is null', () => {
+    const sut = new Required(null as any, 'any_field')
 
-  //   const error = sut.validate()
+    const error = sut.validate()
 
-  //   expect(error).toEqual(new RequiredFieldError('any_field'))
-  // })
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
 
-  // it('should return RequiredFieldError if value is null', () => {
-  //   const sut = new Required(null as any, 'any_field')
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new Required(undefined as any, 'any_field')
 
-  //   const error = sut.validate()
+    const error = sut.validate()
 
-  //   expect(error).toEqual(new RequiredFieldError('any_field'))
-  // })
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
 
-  // it('should return RequiredFieldError if value is undefined', () => {
-  //   const sut = new Required(undefined as any, 'any_field')
-
-  //   const error = sut.validate()
-
-  //   expect(error).toEqual(new RequiredFieldError('any_field'))
-  // })
-
-  it('should return RequiredFieldError if value is not empty', () => {
+  it('should return undefined if value is not empty', () => {
     const sut = new Required('any_value', 'any_field')
 
     const error = sut.validate()
@@ -42,15 +34,15 @@ describe('RequiredString', () => {
     expect(sut).toBeInstanceOf(Required)
   })
 
-  // it('should return RequiredFieldError if value is empty', () => {
-  //   const sut = new RequiredString('', 'any_field')
+  it('should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredString('', 'any_field')
 
-  //   const error = sut.validate()
+    const error = sut.validate()
 
-  //   expect(error).toEqual(new RequiredFieldError('any_field'))
-  // })
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
 
-  it('should return RequiredFieldError if value is not empty', () => {
+  it('should return undefined if value is not empty', () => {
     const sut = new RequiredString('any_value', 'any_field')
 
     const error = sut.validate()
