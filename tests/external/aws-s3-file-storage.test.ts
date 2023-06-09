828c474031eab959ef1c47da985c7cc6e8137dd3
Francisco Passos <chico.81@hotmail.com>
2023-06-09 14:47:17 -0300

    change use case to receive file with mime type

--- tests/external/aws-s3-file-storage.test.ts
+++ tests/external/aws-s3-file-storage.test.ts
@@ -17,13 +17,13 @@ describe('Aws S3 Integration Tests', () => {
   it('should upload and delete image from aws s3', async () => {
     const onePixelImage = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjCFn++j8ABh8C5naNh+cAAAAASUVORK5CYII='
     const file = Buffer.from(onePixelImage, 'base64')
-    const key = 'any_key.png'
+    const fileName = 'any_file_name.png'
 
-    const pictureUrl = await sut.upload({ key, file })
+    const pictureUrl = await sut.upload({ fileName, file })
 
     expect((await axios.get(pictureUrl)).status).toBe(200)
 
-    await sut.delete({ key })
+    await sut.delete({ fileName })
 
     await expect(axios.get(pictureUrl)).rejects.toThrow()
   })
