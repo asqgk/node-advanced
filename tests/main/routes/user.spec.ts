914e1e9e2c00ee2174cfd96a79fc5bd43a45d760
Francisco Passos <chico.81@hotmail.com>
2023-06-20 17:35:38 -0300

    add delete user picture integration test

--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -1,19 +1,22 @@
 import { PgUser } from '@/infra/repos/postgres/entities'
 import { app } from '@/main/config/app'
-import { UnauthorizedError } from '@/application/errors'
+import { env } from '@/main/config/env'
 import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 
 import { IBackup } from 'pg-mem'
-import { getConnection } from 'typeorm'
+import { Repository, getConnection, getRepository } from 'typeorm'
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 
 describe('User Routes', () => {
   describe('Delete /users/picture', () => {
     let backup: IBackup
+    let pgUserRepo: Repository<PgUser>
 
     beforeAll(async () => {
       const db = await makeFakeDb([PgUser])
       backup = db.backup()
+      pgUserRepo = getRepository(PgUser)
     })
 
     afterAll(async () => {
@@ -27,9 +30,20 @@ describe('User Routes', () => {
     it('should return 403 if no authorization header is present', async () => {
       const { status } = await request(app)
         .delete('/api/users/picture')
-        .send()
 
       expect(status).toBe(403)
+    })
+
+    it('should return 204', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email' })
+      const authorization = sign({ key: id }, env.jwtSecret)
+
+      const { status, body } = await request(app)
+        .delete('/api/users/picture')
+        .set({ authorization })
+
+      expect(status).toBe(204)
+      expect(body).toEqual({})
     })
   })
 })
